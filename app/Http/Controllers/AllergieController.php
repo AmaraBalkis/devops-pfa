a75c992e5668e324894d80b5646373eb764442b8
AmaraBalkis <balkis.amara@gmail.com>
2024-05-26 17:58:20 +0100

    modifier le tag pour verifier la mise a jour avec pipline

--- app/Http/Controllers/AllergieController.php
+++ app/Http/Controllers/AllergieController.php
@@ -13,7 +13,7 @@ class AllergieController extends Controller
  * @OA\Get(
  *      path="/api/allergie",
  *      operationId="getallergieList",
- *      tags={"Allergie"},
+ *      tags={"Allergie teter pour le pipine"},
  *      summary="Get list of allergy",
  *      description="Returns list of allergy",
  *      @OA\Response(
